9773f34aa877a805a42a2cbf6ec82d42de952006
Thomas de Graaff <t.de.graaff@vu.nl>
2021-06-15 13:04:00 +0200

    First rough draft

---
+++ .auctex-auto/network_size.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "network_size"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "tikz"
+    "pgfplots"))
+ :latex)
+
---
+++ network_size.x.gnuplot
@@ -0,0 +1,2 @@
+set table "network_size.x.table"; set format "%.5f"
+set samples 25; plot [x=0.1:4] exp(-2)
